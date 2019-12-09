ccc3563bb3e21fe0fded629571347763dbfb02a5
The Sharp Ninja <ninja@thesharp.ninja>
2019-12-09 12:22:58 -0600

    Resolved merge
    Signed-off-by: The Sharp Ninja <ninja@thesharp.ninja>

---
+++ QuickPad.MVVM/Commands/Actions/ShowSettings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QuickPad.Mvvm;
+using QuickPad.Mvvm.Commands;
+using QuickPad.Mvvm.ViewModels;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace QuickPad.MVVM
+{
+    public class ShowSettings : SimpleCommand<DocumentViewModel>
+    {
+        public ShowSettings()
+        {
+            Executioner = viewModel =>
+            {
+                //open settings page
+                _ = viewModel.Settings.ShowSettings != viewModel.Settings.ShowSettings;
+                return Task.CompletedTask;
+            };
+        }
+    }
+
+}
